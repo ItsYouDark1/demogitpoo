eacd64d64506ff82bfcf0b324bfbcaa3b196d6f0
YouDark <dd5295791@gmail.com>
2024-10-31 10:02:11 -0500

    Se crea moto

---
+++ src/Automovil.java
@@ -0,0 +1,13 @@
+public class Automovil extends Vehiculo{
+    public boolean isDescapotable() {
+        return descapotable;
+    }
+
+    public void setDescapotable(boolean descapotable) {
+        this.descapotable = descapotable;
+    }
+
+    private boolean descapotable;
+}
+
+public void
